6d0386e0896f523d2ed70abf3769ca3cf806c9c3
happynet78 <49040138+happynet78@users.noreply.github.com>
2021-08-23 18:39:13 +0900

    Create Repository
    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<!-- <CreatePost />
+	<PostList /> -->
+
+	<!-- Lifecycle Hooks -->
+	<!-- <Parent /> -->
+
+	<TemplateRef />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import PostList from './components/PostList.vue'
+// import CreatePost from './components/CreatePost.vue'
+// import Parent from './components/Parent.vue'
+import TemplateRef from './components/TemplateRef.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		// PostList,
+		// CreatePost,
+		// Parent,
+		TemplateRef,
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/TemplateRef.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<input type="text" ref="inputRef" />
+		<PostList ref="PostListRef" />
+	</div>
+</template>
+
+<script>
+import PostList from './PostList.vue'
+
+	export default {
+		name: 'TemplateRef',
+		components: {
+			PostList,
+		},
+		mounted() {
+			console.log(this.$refs.postListRef)
+			this.$refs.inputRef.focus()
+		},
+	}
+</script>
+
+<style scoped>
+
+</style>
